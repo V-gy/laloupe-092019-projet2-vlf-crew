3db9e13d02209bb6fc2c49ace7aa7e48810b2621
V-gy <vgy@hotmail.fr>
2019-11-18 09:58:32 +0100

    Updates

---
+++ precache-manifest.f7293f719266f01fde2f36c6445a2054.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b72205304f22dd6fd6cb2b5b28f4c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "530e39d603f386ca9336",
+    "url": "/static/css/main.23a2e7e0.chunk.css"
+  },
+  {
+    "revision": "0569ca1a69832dc9ba88",
+    "url": "/static/js/2.cd6cbc32.chunk.js"
+  },
+  {
+    "revision": "530e39d603f386ca9336",
+    "url": "/static/js/main.b69a0ecf.chunk.js"
+  },
+  {
+    "revision": "a405061488a2dd23080e",
+    "url": "/static/js/runtime-main.10e7b1a4.js"
+  }
+]);
